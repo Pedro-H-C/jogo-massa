99523758724d952824abfaf07961791940403ad9
Pedro-H-C <pedrocavalcanti209@gmail.com>
2024-12-21 00:50:06 -0300

    Fazendo testes...

--- LINUXorMACrun.py
+++ LINUXorMACrun.py
@@ -10,8 +10,11 @@ def activate_and_run():
     # Caminho do ambiente virtual (assumindo que "venv" está na raiz do projeto)
     venv_dir = os.path.join(project_dir, "venv")
 
-    # Caminho para o executável Python dentro do ambiente virtual
-    python_executable = os.path.join(venv_dir, "Scripts", "python.exe")
+    # Determina o caminho para o Python no ambiente virtual dependendo do sistema operacional
+    if platform.system() == "Windows":
+        python_executable = os.path.join(venv_dir, "Scripts", "python.exe")
+    else:
+        python_executable = os.path.join(venv_dir, "bin", "python3")
 
     # Caminho para o arquivo main.py
     main_script = os.path.join(project_dir, "main.py")
@@ -19,6 +22,10 @@ def activate_and_run():
         print(f"Erro: Arquivo main.py não encontrado no diretório do projeto ({project_dir}).")
         sys.exit(1)
 
+    # Verifica se o executável Python existe
+    if not os.path.exists(python_executable):
+        print(f"Erro: Executável Python não encontrado no ambiente virtual ({python_executable}).")
+        sys.exit(1)
     try:
         # Executa o script main.py usando o Python do ambiente virtual
         print(f"Usando o Python do ambiente virtual: {python_executable}")
